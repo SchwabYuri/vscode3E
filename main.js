48c9c6635b91f727dc19374ce9f482a6bf22d11e
SchwabYuri <146956851+SchwabYuri@users.noreply.github.com>
2024-05-07 16:34:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ const botoes = document.querySelectorAll(".botao");
 for(let i=0;i<botoes.length;i++){
     botoes[i].onclick=function(){
         botoes[i].classList.add("ativo");
-        
     }
 }
